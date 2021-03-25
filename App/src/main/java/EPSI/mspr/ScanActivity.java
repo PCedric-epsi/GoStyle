2d859a013e873a5a41d091a2ac918024c8b298cd
Galdric PUJOL <55428835+onfire25@users.noreply.github.com>
2021-03-25 11:42:01 +0100

    Add files via upload

---
+++ App/src/main/java/EPSI/mspr/ScanActivity.java
@@ -0,0 +1,19 @@
+package EPSI.mspr;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScanActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
