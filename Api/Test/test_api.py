848c990f3aa0cf6af56da43f677c51d3a5191d97
wilfriedndr <56036508+wilfriedndr@users.noreply.github.com>
2021-03-30 15:21:40 +0200

    update test

--- Api/Test/test_api.py
+++ Api/Test/test_api.py
@@ -5,7 +5,7 @@ import pytest
 ''' test paramétré
     x prends le nom du code
     y prends la value du code
-    il y a 9 noms de code et 9 values de code, onc il y aura 81 tests pour vérifier que 1 code à uniquement 1 nom et 1 value
+    il y a 9 noms de code et 9 values de code, donc il y aura 81 tests pour vérifier que 1 code à uniquement 1 nom et 1 value
     donc on doit retourner 72 test failed et 9 test success'''
 @pytest.mark.parametrize("x", ["SUPER CODE", "TERA CODE", "GIGA CODE", "PETA CODE", "MEGA CODE", "ZETTA CODE", "YOTTA CODE", "EXA CODE", "NOEL CODE"])
 @pytest.mark.parametrize("y", [30, 60 , 50, 70, 40, 90, 100, 80, 25])
